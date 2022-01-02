3b5d5d8ae019f98e6ef57ddd17119ddb42ff3344
Rakesh Narang <rakeshnarang5@gmail.com>
2022-01-02 19:14:45 +0530

    Update ReadAndWriteToConsole.java

--- ReadAndWriteToConsole.java
+++ ReadAndWriteToConsole.java
@@ -4,9 +4,13 @@ import java.util.Scanner;
 
 public class ReadAndWriteToConsole {
     public static void main(String[] args) {
+        // create a scanner object and pass System.in object as constructor argument
         Scanner reader = new Scanner(System.in);
-        while(reader.hasNext()){
+        // while reader has data, keep the loop running
+        while (reader.hasNext()) {
+            // read data to word
             String word = reader.nextLine();
+            // print the word
             System.out.println(word);
         }
     }
